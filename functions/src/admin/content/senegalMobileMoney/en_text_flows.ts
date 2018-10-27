83038f9444fecba533fedeee006be1b1cb398e41
Lewis Daly <lewisdaly@me.com>
2018-10-27 18:25:50 +1030

    adding flow types

--- functions/src/admin/content/senegalMobileMoney/en_text_flows.ts
+++ functions/src/admin/content/senegalMobileMoney/en_text_flows.ts
@@ -8,10 +8,147 @@ import { FlowMap, FlowType, BlockId, AnyFlowMap, SenegalNotificationFlowMap, Sen
 const TwilioFlows: SenegalMobileMoneyFlowMap = {
   'entrypoint': {
     type: FlowType.DEFAULT,
-    next: BlockId.notification_0,
+    next: BlockId.story_1_intro_option,
   },
-  // TODO: Not sure how to end the call...
-  'notification_0': {
+  'story_1_intro_option': {
+    type: FlowType.GATHER,
+    error: BlockId.error_0,
+    digitMatches: [
+      { digits: '1', nextBlock: BlockId.story_1_pin_advice },
+      { digits: '2', nextBlock: BlockId.story_1_guess },
+    ],
+  },
+  'story_1_pin_advice': { 
+    type: FlowType.DEFAULT,
+    next: BlockId.story_1_pin_option,
+  },
+  'story_1_pin_option': { 
+    type: FlowType.GATHER,
+    error: BlockId.error_0,
+    digitMatches: [
+      { digits: '1', nextBlock: BlockId.story_1_customer },
+      { digits: '2', nextBlock: BlockId.story_1_guess },
+    ],
+  },
+  'story_1_guess': { 
+    type: FlowType.DEFAULT,
+    next: BlockId.story_1_guess_option,
+  },
+  'story_1_guess_option': { 
+    type: FlowType.GATHER,
+    error: BlockId.error_0,
+    digitMatches: [
+      { digits: '1', nextBlock: BlockId.story_1_customer },
+      { digits: '2', nextBlock: BlockId.story_1_end },
+    ], 
+  },
+  'story_1_customer': { 
+    type: FlowType.DEFAULT,
+    next: BlockId.story_1_customer_option_2,
+  },
+  'story_1_customer_option_2': { 
+    type: FlowType.GATHER,
+    error: BlockId.error_0,
+    digitMatches: [
+      { digits: '2', nextBlock: BlockId.story_1_end },
+    ], 
+  },
+  'story_1_end': { 
+    type: FlowType.DEFAULT,
+    next: BlockId.story_1_next,
+  },
+  'story_1_next': { 
+    type: FlowType.DEFAULT,
+    next: BlockId.story_2_intro,
+  },
+  'story_2_intro': { 
+    type: FlowType.DEFAULT,
+    next: BlockId.story_2_intro_option,
+  },
+  'story_2_intro_option': { 
+    type: FlowType.GATHER,
+    error: BlockId.error_0,
+    digitMatches: [
+      { digits: '1', nextBlock: BlockId.story_2_explain },
+      { digits: '2', nextBlock: BlockId.story_2_send_explain },
+    ], 
+  },
+  'story_2_explain': { 
+    type: FlowType.DEFAULT,
+    next: BlockId.story_2_explain_option,
+  },
+  'story_2_explain_option': { 
+    type: FlowType.GATHER,
+    error: BlockId.error_0,
+    digitMatches: [
+      { digits: '1', nextBlock: BlockId.story_2_send_explain_2 },
+      { digits: '2', nextBlock: BlockId.story_2_end },
+    ], 
+  },
+  'story_2_customer_care': { 
+    type: FlowType.DEFAULT,
+    next: BlockId.story_2_intro_option,
+  },
+  'story_2_send_no_agent': { 
+    type: FlowType.DEFAULT,
+    next: BlockId.story_2_send_agent_option,
+  },
+  'story_2_send_agent_option': { 
+    type: FlowType.GATHER,
+    error: BlockId.error_0,
+    digitMatches: [
+      { digits: '1', nextBlock: BlockId.story_2_send_explain_2 },
+    ], 
+  },
+  'story_2_send_explain': { 
+    type: FlowType.DEFAULT,
+    next: BlockId.story_2_send_explain_option,
+  },
+  'story_2_send_explain_option': { 
+    type: FlowType.GATHER,
+    error: BlockId.error_0,
+    digitMatches: [
+      { digits: '1', nextBlock: BlockId.story_2_send_explain_2 },
+      { digits: '2', nextBlock: BlockId.story_2_end },
+    ], 
+  },
+  'story_2_send_explain_2': { 
+    type: FlowType.DEFAULT,
+    next: BlockId.story_2_end,
+  },
+  'story_2_end': { 
+    type: FlowType.DEFAULT,
+    next: BlockId.story_2_next,
+  },
+  'story_2_next': { 
+    type: FlowType.DEFAULT,
+    next: BlockId.story_3_intro,
+  },
+  'story_3_intro': { 
+    type: FlowType.DEFAULT,
+    next: BlockId.story_2_intro_option,
+  },
+  'story_3_intro_option': { 
+    type: FlowType.GATHER,
+    error: BlockId.error_0,
+    digitMatches: [
+      { digits: '1', nextBlock: BlockId.story_3_decision },
+      { digits: '2', nextBlock: BlockId.story_3_end },
+    ], 
+  },
+  'story_3_decision': { 
+    type: FlowType.DEFAULT,
+    next: BlockId.story_3_decision_option,
+  },
+  'story_3_decision_option': { 
+    type: FlowType.GATHER,
+    error: BlockId.error_0,
+    digitMatches: [
+      { digits: '1', nextBlock: BlockId.story_3_end },
+    ], 
+  },
+  //End block, won't react entrypoint
+  'story_3_end': { 
     type: FlowType.DEFAULT,
     next: BlockId.entrypoint,
   },
