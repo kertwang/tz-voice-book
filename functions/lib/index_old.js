9f6df96ceae033e19c81bc53058685e0f49dce72
Lewis Daly <lewisdaly@me.com>
2018-10-29 12:48:55 +1030

    reduce function overhead by separating out imports

---
+++ functions/lib/index_old.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const functions = require("firebase-functions");
+const fbAdmin = require('firebase-admin');
+fbAdmin.initializeApp();
+// TODO:
+// Implement something like this:
+// https://github.com/firebase/functions-samples/issues/170#issuecomment-323375462
+const functionName = process.env.FUNCTION_NAME;
+console.log("init for function", functionName);
+exports.admin = require('./handlers/fn_admin')(functions, fbAdmin);
+exports.message = require('./handlers/fn_message')(functions, fbAdmin);
+exports.reset_pin = require('./handlers/fn_reset_pin')(functions, fbAdmin);
+exports.twiml = require('./handlers/fn_twiml')(functions);
+exports.dialogflow = require('./handlers/fn_dialogflow');
+//# sourceMappingURL=index_old.js.map
--- functions/src/index.js
+++ functions/src/index.js
@@ -1,30 +1,39 @@
 const functions = require("firebase-functions");
 const fbAdmin = require('firebase-admin');
 fbAdmin.initializeApp();
+
+
 /**
- * This file works better in JS,
+ * This file works better in JS, 
  * When it is in TS, it gets compiled to JS and this breaks
  * the imports for firebase functions
- *
+ * 
  */
+
 // TODO:
 // Implement something like this:
 // https://github.com/firebase/functions-samples/issues/170#issuecomment-323375462
+
 const functionName = process.env.FUNCTION_NAME;
 console.log("init for function", functionName);
+
 if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'admin') {
-    exports.admin = require('./handlers/fn_admin')(functions, fbAdmin);
+  exports.admin = require('./handlers/fn_admin')(functions, fbAdmin);
 }
+  
 if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'message') {
-    exports.message = require('./handlers/fn_message')(functions, fbAdmin);
+  exports.message = require('./handlers/fn_message')(functions, fbAdmin);
 }
+
 if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'reset_pin') {
-    exports.reset_pin = require('./handlers/fn_reset_pin')(functions, fbAdmin);
+  exports.reset_pin = require('./handlers/fn_reset_pin')(functions, fbAdmin);
 }
+
 if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'twiml') {
-    exports.twiml = require('./handlers/fn_twiml')(functions);
+  exports.twiml = require('./handlers/fn_twiml')(functions);
 }
+
 if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'dialogflow-dialogflowFirebaseFulfillment') {
-    exports.dialogflow = require('./handlers/fn_dialogflow');
+  exports.dialogflow = require('./handlers/fn_dialogflow');
 }
-//# sourceMappingURL=index.js.map
+
