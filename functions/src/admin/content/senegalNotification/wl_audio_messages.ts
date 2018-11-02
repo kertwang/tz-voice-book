88a31bde73e7e358a7c7ad5144a3ce753a9c33aa
Lewis Daly <lewisdaly@me.com>
2018-11-02 23:55:20 +1030

    fix wrong link for mobile money notification

--- functions/src/admin/content/senegalNotification/wl_audio_messages.ts
+++ functions/src/admin/content/senegalNotification/wl_audio_messages.ts
@@ -1,13 +1,18 @@
-import { MessageType, MessageMap, SayMessage, AnyMessageMap, SenegalNotificationMessageMap } from "../../../types_rn/TwilioTypes";
+import { MessageType, MessageMap, SayMessage, AnyMessageMap, SenegalNotificationMessageMap, PlayMessage } from "../../../types_rn/TwilioTypes";
 import { generateUrl } from "../../../utils";
 
 const urlPrefix = `https://www.googleapis.com/download/storage/v1/b/${process.env.storageBucket.replace("\"", '')}/o/`;
 const firebaseToken = '1536715274666696'; //This isn't too precious, our files are public anyway
 
+
+function generatePlay(messageId: string): PlayMessage {
+  return { type: MessageType.PLAY, url: generateUrl(urlPrefix, `wl_audio/${messageId}.mp3`, firebaseToken) }
+}
+
 /* the deploy script will automatically fill in the urls for us */
 const en_text: SenegalNotificationMessageMap = {
   'entrypoint': [
-    { type: MessageType.PLAY, url: generateUrl(urlPrefix, 'wl_audio/W7.mp3', firebaseToken) },
+    generatePlay('W7'),
   ]
 }
 
